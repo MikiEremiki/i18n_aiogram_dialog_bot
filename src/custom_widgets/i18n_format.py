00f00182cf99449537b29cc76a0b1249e0d1eb29
Mikhail Eremin <52360858+MikiEremiki@users.noreply.github.com>
2024-07-09 07:05:14 +0300

    Update i18n_format.py
    remove deprecated list_keys

--- src/custom_widgets/i18n_format.py
+++ src/custom_widgets/i18n_format.py
@@ -7,12 +7,9 @@ from fluentogram import TranslatorRunner
 
 
 class I18NFormat(Text):
-    def __init__(self, text: str,
-                 list_keys: List[str] | None = None,
-                 when: WhenCondition = None):
+    def __init__(self, text: str, when: WhenCondition = None):
         super().__init__(when)
         self.text = text
-        self.list_keys = list_keys
 
     async def _render_text(self, data: Dict, manager: DialogManager) -> str:
         i18n: TranslatorRunner = manager.middleware_data.get('i18n')
